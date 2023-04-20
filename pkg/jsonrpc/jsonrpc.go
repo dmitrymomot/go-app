c6cf5974a1f04e96dbfb388647c4a5aeda34e39e
Dmitry Momot <mail@dmomot.com>
2023-04-20 17:32:31 +0300

    + parse request

--- pkg/jsonrpc/jsonrpc.go
+++ pkg/jsonrpc/jsonrpc.go
@@ -1,5 +1,10 @@
 package jsonrpc
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 // NewJSONRPCResponse creates new JSON-RPC 2.0 response
 func NewJSONRPCResponse(id interface{}, result interface{}) *JSONRPCResponse {
 	return &JSONRPCResponse{
@@ -39,4 +44,31 @@ func NewJSONRPCNotification(method string, params interface{}) *JSONRPCRequest {
 		Method:  method,
 		Params:  params,
 	}
+}
+
+// ParseRequest parses JSON-RPC 2.0 request from JSON string
+func ParseRequest(data []byte) (*JSONRPCRequest, error) {
+	var req JSONRPCRequest
+	if err := json.Unmarshal(data, &req); err != nil {
+		return nil, err
+	}
+	return &req, nil
+}
+
+// ParseResponse parses JSON-RPC 2.0 response from JSON string
+func ParseResponse(data []byte) (*JSONRPCResponse, error) {
+	var res JSONRPCResponse
+	if err := json.Unmarshal(data, &res); err != nil {
+		return nil, err
+	}
+	return &res, nil
+}
+
+// ParseHTTPRequest parses JSON-RPC 2.0 request from HTTP request
+func ParseHTTPRequest(req *http.Request) (*JSONRPCRequest, error) {
+	var jsonReq JSONRPCRequest
+	if err := json.NewDecoder(req.Body).Decode(&jsonReq); err != nil {
+		return nil, err
+	}
+	return &jsonReq, nil
 }
